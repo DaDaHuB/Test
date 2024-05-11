60685c023d21c4dc330c680be3feb71e7c379991
DaDaHub <hzw18911868538@gmail.com>
2024-05-11 15:34:58 +0800

    第一次提交本地

---
+++ src/main/java/org/dada/Main.java
@@ -0,0 +1,16 @@
+package org.dada;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        System.out.println(value);
+        if(value == 1){
+            System.out.println("Hello world!");
+        }else{
+            System.out.println("No world");
+        }
+    }
+}
